930991f476f50cd40a8c256619163693c763a79d
CuteFox87 <leo945394dj6@gmail.com>
2025-05-31 18:31:58 +0800

    fix some docker error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'http://backend:3000'
     }
   }
 })
